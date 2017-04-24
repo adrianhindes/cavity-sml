5ec128c319bbb033c1a24414a910d8171cc77e2f
adrianhindes <adrian@adrianhindes.com>
2017-04-24 16:15:59 +1000

    Backported createData.py to Python 2, merged code with generateData.py

--- createData.py
+++ createData.py
@@ -4,18 +4,43 @@ import shutil
 from skimage import io, util
 import os
 from PIL import Image
+import tqdm
+from keras.preprocessing.image import ImageDataGenerator, array_to_img, \
+    img_to_array, load_img
 
 
+'''
+Simple python script to generate data from Finesse
+First we run Finesse iterated over mode numbers, m, n
+(Just pure modes, no superpositions)
+Fix up the GNUplot raw image
+Then use Keras preprocessing package to generate modified data
+(rotated, skewed, zoomed)
+Then we add noise (after Keras, otherwise wacky things happen)
+Ready to feed into neural network
+'''
+FINESSEDIR = "../../finesse/./kat"
 # saturation levels
 colourRange = ['2', '2e-1', '2e-2', '2e-3', '2e-4']
 colourName = ['0', '1', '2', '3', '4']
-# max modes to generate
+# max mode for n and m to generate
 maxMode = 6
 # open template kat file
 originalKat = open('cavity.kat', 'r')
 
+# Define data folders
+dataFolder = 'rawData'
+newDataFolder = 'newData'
+
+# Image extension
+ext = '.png'
+
+# No. images to generate per raw image
+imageNum = 20
+
+
 # loop over m, then n, then colour range
-for m in range(maxMode):
+for m in tqdm(range(maxMode)):
     for n in range(maxMode):
         for c in range(5):
 
@@ -24,7 +49,7 @@ for m in range(maxMode):
                 generateFileName = current+".kat"
 
                 # make and/or change directory
-                if not os.path.exists('rawData'): os.mkdir('rawData')
+                if not os.path.exists(dataFolder): os.mkdir(dataFolder)
                 shutil.copy("cavity.kat", 'rawData/'+generateFileName)
                 os.chdir('rawData')
 
@@ -43,24 +68,78 @@ for m in range(maxMode):
                             print(line)
 
                 # run the new kat file
-                subprocess.run(["../../finesse/./kat", generateFileName])
+                subprocess.run([FINESSEDIR, generateFileName])
 
                 # crop generated image
                 # GNUplot generates a margin for some reason
                 # assume square image
-                image = Image.open(current+'.png')
+                image = Image.open(current+ext)
                 width = image.size[0]
                 height = width
                 offset = height-1
                 cropped = image.crop((width-offset, height-offset, width, height))
-                cropped.save(current+'.png')
+                cropped.save(current+ext)
 
                 # cleaning files
                 fileList = os.listdir()
-                notImages = [x for x in fileList if not '.png' in x]
+                notImages = [x for x in fileList if not ext in x]
                 for fileName in notImages: os.remove(fileName)
 
                 #move back up directory
                 os.chdir('..')
 
 originalKat.close()
+
+# Preprocessing
+# Data generators, using Keras
+trainDatagen = ImageDataGenerator(
+        rotation_range=30,
+        width_shift_range=0.2,
+        height_shift_range=0.2,
+        shear_range=0.2,
+        zoom_range=0.2,
+        horizontal_flip=True,
+        fill_mode='nearest')
+testDatagen = ImageDataGenerator(
+        rotation_range=25,
+        width_shift_range=0.1,
+        height_shift_range=0.1,
+        shear_range=0.3,
+        zoom_range=0.1,
+        horizontal_flip=True,
+        fill_mode='nearest')
+#
+os.chdir(dataFolder)
+files = os.listdir()
+imageList = [x for x in files if '.png' in x]
+os.chdir('..')
+
+possibleModes = [str(m)+str(n) for m in range(maxMode) for n in range(maxMode)]
+
+# Create file structure if it does not exist
+if not os.path.exists(newDataFolder): os.mkdir(newDataFolder)
+
+# Generators which read pictures in subfolders of training and validation,
+# indefinitely generate batches of augmented cavity images
+
+print('Preprocessing raw data set')
+for mode in tqdm(possibleModes):
+       for image in [x for x in files if 'cavity'+mode in x]:
+        loaded = load_img('rawData/'+image)
+        array = img_to_array(loaded)
+        array = array.reshape((1,)+array.shape)
+        i = 0
+        for batch in trainDatagen.flow(array, batch_size=1, save_to_dir=newDataFolder,
+                                       save_prefix=mode, save_format=ext):
+            i += 1
+            if i > imageNum: break
+        print('generated '+image+' set')
+
+# Adding noise
+def noisy(img):
+    noisy = util.random_noise(loaded, mode='gaussian', clip=True)
+    return noisy
+
+for image in tqdm(newDataFolder):
+    loaded = io.imread(newDataFolder+'/'+image)
+    io.imsave(image, noisy(image))
