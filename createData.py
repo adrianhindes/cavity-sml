a18199e0aa6356496287d103f1e2fea7840fd068
adrianhindes <adrian@adrianhindes.com>
2017-04-24 16:33:08 +1000

    tqdm import fix

--- createData.py
+++ createData.py
@@ -11,7 +11,7 @@ import os
 # Cropping raw images
 from PIL import Image
 # Nice loading bars for loops
-import tqdm
+from tqdm import tqdm
 # Keras preprocessing package for augmentation
 from keras.preprocessing.image import ImageDataGenerator, array_to_img, \
     img_to_array, load_img
